4ba1aed9a9d26b10efe1900a8ede8f228282eff8
euzin4 <tste@outlook.com.br>
2017-09-05 00:22:02 -0300

    trabalho lista 1

---
+++ exe1.java
@@ -0,0 +1,8 @@
+class exe1{
+	public static void main(String[] args){
+		int i;
+		for(i=120;i<281;i++){
+			System.out.println(i);
+		}
+	}
+}
---
+++ exe2.java
@@ -0,0 +1,9 @@
+class exe2{
+	public static void main(String[] args){
+		int i,val=0;
+		for(i=0;i<500;i++){
+			++val;
+			System.out.println(val);
+		}
+	}
+}
