351e6d6ab10c06029de482d387dde89520aae755
euzin4 <tste@outlook.com.br>
2017-09-16 15:07:46 -0300

    conteudoAula2

---
+++ Pessoa.java
@@ -0,0 +1,12 @@
+public class Pessoa{
+	String nomeAutor;
+	String emailAutor;
+	String cpfAutor;
+
+	public void mostraDetalhes1(){
+			System.out.println("\nDetalhes");
+			System.out.println("Nome: "+this.nomeAutor);
+			System.out.println("Email: "+this.emailAutor);
+			System.out.println("CPF: "+this.cpfAutor+"\n");
+	}
+}
---
+++ Teste.java
@@ -0,0 +1,11 @@
+public class Teste{
+	public static void main(String[] args){
+		Livro livrinho = new Livro();
+		livrinho.nome = "nome do livro";
+
+		livrinho.autor.nome="nome do autor";
+		livrinho.valor=123;
+		livrinho.descricao="descricao aqui";
+		livrinho.isbn="123123123";
+	}
+}
