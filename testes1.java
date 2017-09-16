9032a58ff730553195804bde36bed0f5f7441568
euzin4 <tste@outlook.com.br>
2017-09-16 15:45:52 -0300

    usando scanner

---
+++ testes1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class testes1{
+	public static void main(String[]args){
+		Scanner entrada=new Scanner(System.in);
+		int x;
+		System.out.print("digite um valor:");
+		x=entrada.nextInt();
+		System.out.println("o valor era: "+x);
+	}
+}
