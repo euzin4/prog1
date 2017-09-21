9bdc70ce93021ee0bdf3027d363cf4517af25c27
euzin4 <tste@outlook.com.br>
2017-09-21 20:17:45 -0300

    210917 pop up com input

---
+++ Dialogo1.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+
+public class Dialogo1{
+	public static void main(String[]args){
+		String nome=JOptionPane.showInputDialog("Qual é o seu nome?");
+		JOptionPane.showMessageDialog(null,"Olá "+nome+"!");
+	}
+}
