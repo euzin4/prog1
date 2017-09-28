8add2e6563369262af4e0f7268a8a1f06c8acef3
euzin4 <tste@outlook.com.br>
2017-09-28 20:44:53 -0300

    aula

--- Pessoa.java
+++ Pessoa.java
@@ -1,12 +1,7 @@
-public class Pessoa{
-	String nomeAutor;
-	String emailAutor;
-	String cpfAutor;
+package aula;
 
-	public void mostraDetalhes1(){
-			System.out.println("\nDetalhes");
-			System.out.println("Nome: "+this.nomeAutor);
-			System.out.println("Email: "+this.emailAutor);
-			System.out.println("CPF: "+this.cpfAutor+"\n");
-	}
+public class Pessoa {
+    int idade;
+    float peso;
+    String nome;    //terminar
 }
---
+++ aula280917.java
@@ -0,0 +1,8 @@
+package aula;
+
+class aula280917{
+    public static void main (String [] args)
+    {
+        //terminar
+    }
+}
