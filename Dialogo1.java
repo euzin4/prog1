ca651976d3b6766c47cef39e0778d25132b8e9db
kevin willian <tste@outlook.com.br>
2017-10-03 16:45:49 -0300

    Delete Dialogo1.java

--- Dialogo1.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JOptionPane;
-
-public class Dialogo1{
-	public static void main(String[]args){
-		String nome=JOptionPane.showInputDialog("Qual é o seu nome?");
-		JOptionPane.showMessageDialog(null,"Olá "+nome+"!");
-	}
-}
