c1811e9d9a645854b4340b27ceee7fe1cfbbe611
kevin willian <tste@outlook.com.br>
2017-12-18 18:28:19 -0200

    edt

---
+++ trabalhos/trabpg1/Appdecadastros/app/src/main/java/com/example/kevin/appdecadastros/dominio/entidades/Cliente.java
@@ -0,0 +1,13 @@
+package com.example.kevin.appdecadastros.dominio.entidades;
+
+/**
+ * Created by kevin on 30/11/2017.
+ */
+
+public class Cliente {
+    public int codigo;
+    public String nome;
+    public String endereco;
+    public String email;
+    public String telefone;
+}
